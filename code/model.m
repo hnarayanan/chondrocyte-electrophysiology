e52ae6bf2f1ed9a71b2d18dbbeb1e3cf4e04d6f8
Harish Narayanan <harish@simula.no>
2011-01-25 13:14:06 +0100

    Allow for changes in sodium concentration

--- code/model.m
+++ code/model.m
@@ -13,18 +13,22 @@ function xdot = f(x, t)
 
   xdot = zeros(4, 1);
 
-  V = x(1);
-  m = x(2);
-  h = x(3);
-  n = x(4);
+  V    = x(1);
+  Na_i = x(2);
+  Na_c = x(3);
+  n     = x(4);
 
   # Membrane capacitance
   C_m = 15.0;
 
+  # Cellular volume
+  vol_i = 0.005884;
+  vol_c = 0.000800224;
+
   # External stimulation
   t_stim = 0.1;
   t_cycle = 0.5;
-  I_stim_bar = 100.0;
+  I_stim_bar = 0.0;
 
   # Background sodium
   g_Na_b_bar = 20;
@@ -40,7 +44,6 @@ function xdot = f(x, t)
   I_NaK_bar = 68.55;
   K_c = 5.560224;
   K_NaK_K = 1.0;
-  Na_i = 8.516766;
   K_NaK_Na = 11.0;
   I_NaK = I_NaK_bar*(K_c/(K_c + K_NaK_K))*(Na_i^1.5/(Na_i^1.5 + K_NaK_Na^1.5))*(V + 150.0)/(V + 200.0);
 
@@ -51,7 +54,6 @@ function xdot = f(x, t)
   F = 96487.0;
   R = 8314.0;
   T = 306.15;
-  Na_c = 130.022096;
   Ca_i = 6.5e-5;
   d_NaCa = 0.0003;
   I_NaCa = K_NaCa*(Na_i^3*Ca_c*exp(gamma_Na*V*F/(R*T)) - Na_c^3*Ca_i*exp((gamma_Na - 1.0)*V*F/(R*T))) \
@@ -79,18 +81,17 @@ function xdot = f(x, t)
   # External stimulation
   I_stim = I_stim_bar*square(t*2*pi/t_cycle, t_stim/t_cycle);
 
-  alpha_m = 0.1*(V + 25)/(exp((V + 25)/10) - 1);
-  beta_m = 4*exp(V/18);
-
-  alpha_h = 0.07*exp(V/20);
-  beta_h = 1/(exp((V + 30)/10) + 1);
+  # Changes in concentration
+  tau_Na = 0.01;
+  Na_i_dot = - (I_Na_b + 3*I_NaK + 3*I_NaCa + I_NaH)/(vol_i*F);
+  Na_c_dot = (Na_i - Na_c)/tau_Na + (I_Na_b + 3*I_NaK + 3*I_NaCa + I_NaH)/(vol_c*F);
 
   alpha_n = 0.01*(V + 10)/(exp((V + 10)/10) - 1);
   beta_n = 0.125*exp(V/80);
 
   xdot(1) = 1/C_m*(-I_i + I_stim);
-  xdot(2) = alpha_m*(1 - m) - beta_m*m;
-  xdot(3) = alpha_h*(1 - h) - beta_h*h;
+  xdot(2) = Na_i_dot;
+  xdot(3) = Na_c_dot;
   xdot(4) = alpha_n*(1 - n) - beta_n*n;
 
 endfunction
@@ -100,19 +101,19 @@ t_final = 4.0
 dt = 0.005
 
 % Initial conditions
-V0 = -86
-m0 =  1
-h0 =  0
+V0 = -60
+Na_i_0 = 8.516766
+Na_c_0 = 130.022096
 n0 =  1
 
 t = linspace(0, t_final, t_final/dt);
-x0 = [V0, m0, h0, n0];
+x0 = [V0, Na_i_0, Na_c_0, n0];
 x = lsode("f", x0, t);
 
 clf
 figure(1)
-plot(t, x(:,1), "linewidth", 4)
+plot(t, x(:, 1), "linewidth", 4)
 xlabel("Time (s)")
 ylabel("Membrane voltage (mV)")
-# print -depsc2 "membrane_voltage.eps"
+print -depsc2 "membrane_voltage.eps"
 print -depslatex "membrane_voltage.tex"
