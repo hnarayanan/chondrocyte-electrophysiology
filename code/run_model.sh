70cced03b3414211c09e62c44ac264a792bf1bb0
Harish Narayanan <harish@simula.no>
2011-03-17 13:00:41 +0100

    Added a run script

---
+++ code/run_model.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+rm -fr output/*
+octave driver.m
+cd output
+for i in $( ls *.tex ); do
+    perl -pi -e 's/output\//eps\//' ${i}
+done
+mkdir eps
+mv *.eps eps
+
+for i in $( ls *.tex ); do
+    pdflatex ${i}
+done
+rm *.log *.aux
+mkdir tex
+mv *.tex tex
+mkdir pdf
+mv *.pdf pdf
+cd ..
+open output/pdf/*.pdf
