7ebc7b3512253508459fc0aa6f9a308b95a0a3ba
Harish Narayanan <harish@simula.no>
2011-04-04 12:11:57 +0200

    Plot data individually

--- code/plot_solutions.m
+++ code/plot_solutions.m
@@ -8,48 +8,84 @@ line_width = 3;
 V_ref = csvread('references/Total_Current_Density_vs_Membrane_Voltage.data')(:, 1);
 I_i_by_C_m_ref = csvread('references/Total_Current_Density_vs_Membrane_Voltage.data')(:, 2);
 
-# The membrane voltage and total currents
-figure(1, 'visible', 'off');
-subplot(2, 2, 1), plot(t, V, 'linewidth', line_width), xlabel('$t (s)$'), legend('$V_{m} (mV)$');
-subplot(2, 2, 2), plot(t, I_i, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_i (pA)$');
-subplot(2, 2, 3), plot(V, I_i/C_m, 'linewidth', line_width), xlabel('$V_{m} (mV)$'), legend('$I_i/C_{m} (pA/pF)$');
+# Hide plots
+figure(1, 'visible', 'off')
+
+# Plot membrane voltage vs time
+plot(t, V, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$V_{m} (mV)$');
+print -depslatexstandalone "output/t_V.tex"
+
+# Plot total ionic current vs time
+plot(t, I_i, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_i (pA)$');
+print -depslatexstandalone "output/t_I_i.tex"
+
+# Plot total ionic current density vs time (including reference values)
+plot(V, I_i/C_m, 'linewidth', line_width), xlabel('$V_{m} (mV)$'), ylabel('$I_i/C_{m} (pA/pF)$');
 hold on;
 plot(V_ref, I_i_by_C_m_ref, '1', 'linewidth', line_width);
-print -depslatexstandalone "output/membrane_behaviour.tex"
+hold off;
+print -depslatexstandalone "output/V_I_i.tex"
 
-# The different concentrations
-figure(2, 'visible', 'off');
-subplot(2, 2, 1), plot(t, Na_i, 'linewidth', line_width), xlabel('$t (s)$'), legend('$[Na^{+}]_{i} (mM/l)$');
-subplot(2, 2, 2), plot(t, K_i, 'linewidth', line_width), xlabel('$t (s)$'), legend('$[K^{+}]_{i} (mM/l)$');
-subplot(2, 2, 3), plot(t, Ca_i, 'linewidth', line_width), xlabel('$t (s)$'), legend('$[Ca^{2+}]_{i} (mM/l)$');
-print -depslatexstandalone "output/concentrations.tex"
+# Plot interior sodium concentration vs time
+plot(t, Na_i, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$[Na^{+}]_{i} (mM/l)$');
+print -depslatexstandalone "output/t_Na_i.tex"
 
-# The different background currents
-figure(3, 'visible', 'off');
-subplot(2, 2, 1), plot(t, I_Na_b, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{Na_{b}} (pA)$');
-subplot(2, 2, 2), plot(t, I_K_b, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{b}} (pA)$');
-print -depslatexstandalone "output/background_currents.tex"
+# Plot interior potassium concentration vs time
+plot(t, K_i, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$[K^{+}]_{i} (mM/l)$');
+print -depslatexstandalone "output/t_K_i.tex"
 
-# The different pump and exchanger currents
-figure(4, 'visible', 'off');
-subplot(2, 2, 1), plot(t, I_NaK, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{NaK} (pA)$');
-subplot(2, 2, 2), plot(t, I_NaCa, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{NaCa} (pA)$');
-subplot(2, 2, 3), plot(t, I_NaH, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{NaH} (pA)$');
-print -depslatexstandalone "output/pumps_and_exchangers.tex"
+# Plot interior calcium concentration vs time
+plot(t, Ca_i, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$[Ca^{2+}]_{i} (mM/l)$');
+print -depslatexstandalone "output/t_Ca_i.tex"
 
-# The other potassium currents
-figure(5, 'visible', 'off');
-subplot(2, 2, 1), plot(t, I_K_ur, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{ur}} (pA)$');
-subplot(2, 2, 2), plot(t, I_K_2pore, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{2pore}} (pA)$');
-subplot(2, 2, 3), plot(t, I_K_Ca_act, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{Ca-act}} (pA)$');
-subplot(2, 2, 4), plot(t, I_K_ATP, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{ATP}} (pA)$');
-print -depslatexstandalone "output/potassium_currents.tex"
+# Plot background sodium current vs time
+plot(t, I_Na_b, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{Na_{b}} (pA)$');
+print -depslatexstandalone "output/t_I_Na_b.tex"
 
-# The other currents
-figure(6, 'visible', 'off');
-subplot(2, 2, 1), plot(t, I_stim, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{stim} (pA)$');
-subplot(2, 2, 2), plot(t, I_ASIC, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{ASIC} (pA)$');
-subplot(2, 2, 3), plot(t, I_TRP1, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{TRP1} (pA)$');
-subplot(2, 2, 4), plot(t, I_TRP2, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{TRP2} (pA)$');
+# Plot background potassium current vs time
+plot(t, I_K_b, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{K_{b}} (pA)$');
+print -depslatexstandalone "output/t_I_K_b.tex"
 
-print -depslatexstandalone "output/other_currents.tex"
+# Plot sodium-potassium pump current vs time
+plot(t, I_NaK, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{NaK} (pA)$');
+print -depslatexstandalone "output/t_I_NaK.tex"
+
+# Plot sodium-calcium exchanger current vs time
+plot(t, I_NaCa, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{NaCa} (pA)$');
+print -depslatexstandalone "output/t_I_NaCa.tex"
+
+# Plot sodium-hydrogen antiport current vs time
+plot(t, I_NaH, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{NaH} (pA)$');
+print -depslatexstandalone "output/t_I_NaH.tex"
+
+# Plot ultra-rapidly rectifying potassium current vs time
+plot(t, I_K_ur, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{K_{ur}} (pA)$');
+print -depslatexstandalone "output/t_I_K_ur.tex"
+
+# Plot two-pore potassium current vs time
+plot(t, I_K_2pore, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{K_{2pore}} (pA)$');
+print -depslatexstandalone "output/t_I_K_2pore.tex"
+
+# Plot calcium-activated potassium current vs time
+plot(t, I_K_Ca_act, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{K_{Ca-act}} (pA)$');
+print -depslatexstandalone "output/t_I_K_Ca_act.tex"
+
+# Plot ATP-powered potassium pump current vs time
+plot(t, I_K_ATP, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{K_{ATP}} (pA)$');
+print -depslatexstandalone "output/t_I_K_ATP.tex"
+
+# Plot external stimulation current vs time
+plot(t, I_stim, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{stim} (pA)$');
+print -depslatexstandalone "output/t_I_stim.tex"
+
+# Plot voltage-activated hydrogen current vs time
+plot(t, I_ASIC, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{ASIC} (pA)$');
+print -depslatexstandalone "output/t_I_ASIC.tex"
+
+# Plot stretch-activated trip current vs time
+plot(t, I_TRP1, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{TRP1} (pA)$');
+print -depslatexstandalone "output/t_I_TRP1.tex"
+
+# Plot osteo-arthritic trip current vs time
+plot(t, I_TRP2, 'linewidth', line_width), xlabel('$t (s)$'), ylabel('$I_{TRP2} (pA)$');
+print -depslatexstandalone "output/t_I_TRP2.tex"
