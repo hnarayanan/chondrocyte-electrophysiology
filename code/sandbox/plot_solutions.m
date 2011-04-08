e28a269b0c45fe0e852508c40045463e6d8ff8b8
Harish Narayanan <harish@simula.no>
2011-04-08 21:58:49 +0200

    Reverted to older grouping of plots and many fixes related to moving files around

--- code/sandbox/plot_solutions.m
+++ code/sandbox/plot_solutions.m
@@ -5,8 +5,8 @@ clf;
 line_width = 3;
 
 # Load reference solution
-V_ref = csvread('references/Total_Current_Density_vs_Membrane_Voltage.data')(:, 1);
-I_i_by_C_m_ref = csvread('references/Total_Current_Density_vs_Membrane_Voltage.data')(:, 2);
+V_ref = csvread('../data/reference_values/Total_Current_Density_vs_Membrane_Voltage.data')(:, 1);
+I_i_by_C_m_ref = csvread('../data/reference_values/Total_Current_Density_vs_Membrane_Voltage.data')(:, 2);
 
 # The membrane voltage and total currents
 figure(1, 'visible', 'off');
@@ -15,27 +15,27 @@ subplot(2, 2, 2), plot(t, I_i, 'linewidth', line_width), xlabel('$t (s)$'), lege
 subplot(2, 2, 3), plot(V, I_i/C_m, 'linewidth', line_width), xlabel('$V_{m} (mV)$'), legend('$I_i/C_{m} (pA/pF)$');
 hold on;
 plot(V_ref, I_i_by_C_m_ref, '1', 'linewidth', line_width);
-print -depslatexstandalone "output/membrane_behaviour.tex"
+print -depslatexstandalone "../results/epslatex/membrane_behaviour.tex"
 
 # The different concentrations
 figure(2, 'visible', 'off');
 subplot(2, 2, 1), plot(t, Na_i, 'linewidth', line_width), xlabel('$t (s)$'), legend('$[Na^{+}]_{i} (mM/l)$');
 subplot(2, 2, 2), plot(t, K_i, 'linewidth', line_width), xlabel('$t (s)$'), legend('$[K^{+}]_{i} (mM/l)$');
 subplot(2, 2, 3), plot(t, Ca_i, 'linewidth', line_width), xlabel('$t (s)$'), legend('$[Ca^{2+}]_{i} (mM/l)$');
-print -depslatexstandalone "output/concentrations.tex"
+print -depslatexstandalone "../results/epslatex/concentrations.tex"
 
 # The different background currents
 figure(3, 'visible', 'off');
 subplot(2, 2, 1), plot(t, I_Na_b, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{Na_{b}} (pA)$');
 subplot(2, 2, 2), plot(t, I_K_b, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{b}} (pA)$');
-print -depslatexstandalone "output/background_currents.tex"
+print -depslatexstandalone "../results/epslatex/background_currents.tex"
 
 # The different pump and exchanger currents
 figure(4, 'visible', 'off');
 subplot(2, 2, 1), plot(t, I_NaK, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{NaK} (pA)$');
 subplot(2, 2, 2), plot(t, I_NaCa, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{NaCa} (pA)$');
 subplot(2, 2, 3), plot(t, I_NaH, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{NaH} (pA)$');
-print -depslatexstandalone "output/pumps_and_exchangers.tex"
+print -depslatexstandalone "../results/epslatex/pumps_and_exchangers.tex"
 
 # The other potassium currents
 figure(5, 'visible', 'off');
@@ -43,7 +43,7 @@ subplot(2, 2, 1), plot(t, I_K_ur, 'linewidth', line_width), xlabel('$t (s)$'), l
 subplot(2, 2, 2), plot(t, I_K_2pore, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{2pore}} (pA)$');
 subplot(2, 2, 3), plot(t, I_K_Ca_act, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{Ca-act}} (pA)$');
 subplot(2, 2, 4), plot(t, I_K_ATP, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{K_{ATP}} (pA)$');
-print -depslatexstandalone "output/potassium_currents.tex"
+print -depslatexstandalone "../results/epslatex/potassium_currents.tex"
 
 # The other currents
 figure(6, 'visible', 'off');
@@ -52,4 +52,4 @@ subplot(2, 2, 2), plot(t, I_ASIC, 'linewidth', line_width), xlabel('$t (s)$'), l
 subplot(2, 2, 3), plot(t, I_TRP1, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{TRP1} (pA)$');
 subplot(2, 2, 4), plot(t, I_TRP2, 'linewidth', line_width), xlabel('$t (s)$'), legend('$I_{TRP2} (pA)$');
 
-print -depslatexstandalone "output/other_currents.tex"
+print -depslatexstandalone "../results/epslatex/other_currents.tex"
