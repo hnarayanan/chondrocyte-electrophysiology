d7c90ed735db3a58e3aeb014b0221a5fbf5896d6
Harish Narayanan <harish@simula.no>
2011-06-28 12:07:10 +0200

    Added missing copyright headers to new files

--- code/measurederiv.m
+++ code/measurederiv.m
@@ -1,3 +1,12 @@
+# This file is part of the chondrocyte modelling project at Simula
+# Research Laboratory, Norway. Refer to the files README and COPYING for
+# more information about the project as well as terms of distribution.
+#
+# Copyright (C) 2010--2011  Harish Narayanan
+# Licensed under the GNU GPL Version 3
+
+# Define the derivative of the measured function(s) with respect to the
+# different parameters
 function retval = measurederiv(x)
     global z_K, global K_o, global g_K_b_bar;
     global R, global T, global F, global z_K;
--- code/measurefcn.m
+++ code/measurefcn.m
@@ -1,3 +1,11 @@
+# This file is part of the chondrocyte modelling project at Simula
+# Research Laboratory, Norway. Refer to the files README and COPYING for
+# more information about the project as well as terms of distribution.
+#
+# Copyright (C) 2010--2011  Harish Narayanan
+# Licensed under the GNU GPL Version 3
+
+# Define the measured function(s)
 function retval = measurefcn(x)
     global g_K_b_bar;
     V = x(1);
