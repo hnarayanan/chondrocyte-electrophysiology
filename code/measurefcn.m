8be1528a3560a35bee03ce10f4c0adb96f1b853b
Harish Narayanan <harish@simula.no>
2011-06-28 11:35:49 +0200

    Use background current definition instead of reproducing it in the objective function

--- code/measurefcn.m
+++ code/measurefcn.m
@@ -1,7 +1,6 @@
 function retval = measurefcn(x)
-    global z_K, global K_o, global g_K_b_bar;
+    global g_K_b_bar;
     V = x(1);
     K_i = x(2);
-    E_K = nernstPotential(z_K, K_i, K_o);
-    retval = g_K_b_bar*(V - E_K);
+    retval = backgroundPotassium(V, K_i, g_K_b_bar);
 endfunction
